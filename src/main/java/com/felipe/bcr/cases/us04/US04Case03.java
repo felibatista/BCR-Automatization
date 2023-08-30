c0f5bedec087a410dfdf032b064f22f78cdaeb82
Felipe Batista <83935256+felibatista@users.noreply.github.com>
2023-08-30 18:20:31 -0300

    feat: test 3 was added

---
+++ src/main/java/com/felipe/bcr/cases/us04/US04Case03.java
@@ -0,0 +1,10 @@
+package com.felipe.bcr.cases.us04;
+
+public class US04Case03 {
+     /*
+     Mercado Libre cambia constantemente la ubicación del corazón de favoritos,
+     por lo que validar que el mismo se encuentre en la posición pedida se podría hacer
+     si se conocieran todas las posibles ubicaciones. Llevaría más tiempo de lo disponible terminarlo.
+     Por lo que se optó por dejar sin hacer esta prueba de manera automatizada.
+     */
+}
