79f995457ecd89bd0a2710ee5f4063f4beee18b5
Felipe Batista <83935256+felibatista@users.noreply.github.com>
2023-08-30 13:46:34 -0300

    feat: case 6 was added and tested

---
+++ src/main/java/com/felipe/bcr/cases/us02/US02Case06.java
@@ -0,0 +1,85 @@
+package com.felipe.bcr.cases.us02;
+
+import com.felipe.bcr.Case;
+import com.felipe.bcr.Element;
+import com.felipe.bcr.Main;
+import com.felipe.bcr.Status;
+import com.felipe.bcr.controller.CaptchaController;
+import org.openqa.selenium.NoSuchElementException;
+
+import java.time.Duration;
+
+/*
+
+Caso 6 (Positivo):
+    ID: 006
+    Descripción: Validar que se pueda iniciar sesión con correo electrónico existente en la base de datos.
+    Pre-condiciones:
+    1. Abrir: https://www.mercadolibre.com.ar/
+    2. No estar logueado.
+    Entradas:
+    Email válido: “felipebcr@gmail.com”
+    Pasos:
+    Hacer click en el botón “Ingresar” de la parte superior de la pantalla.
+    En el formulario abierto escribir el “Email” en la sección de “Email, teléfono o usuario”
+    Realizar el Captcha en caso de ser solicitado.
+    Hacer click en el botón de “Continuar” para seguir el proceso.
+    Resultados esperados: Se debería abrir un nuevo formulario para introducir la contraseña.
+    Condiciones posteriores: N/A
+ */
+public class US02Case06 {
+    public static void run(){
+        Case caseToTest = new Case(
+                6,
+                "Validar que se pueda iniciar sesión con correo electrónico existente en la base de datos",
+                Status.NOT_EXECUTED
+        );
+
+        //prevent to run if user is logged
+        if (!Main.getLogginController().checkIsLoggedWithJoinButton()) {
+            caseToTest.setStatus(Status.PRE_CONDITION_FAILED);
+            return;
+        }
+
+        try {
+            Element.JOIN_BUTTON.getElement().click();
+
+            Main.getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
+
+            Element.USERNAME_INPUT.getElement().sendKeys("hellojavaa@gmail.com");
+
+            Main.getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
+
+            Element.CONTINUE_BUTTON.getElement().click();
+
+            Main.getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
+
+            //wait for captcha to be filled
+            while (CaptchaController.hasCaptcha()){
+                System.out.println("Esperando acción de rellenado de captcha manual...");
+
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            System.out.println("Captcha rellenado correctamente");
+
+            Main.getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
+
+            //check if password input is present, if not, the email is not registered
+            Element.PASSWORD_INPUT.getElement();
+        } catch (NoSuchElementException e) {
+            caseToTest.setStatus(Status.BLOCKED);
+            return;
+        } catch (Exception e) {
+            e.printStackTrace();
+            caseToTest.setStatus(Status.FAILED);
+            return;
+        }
+
+        caseToTest.setStatus(Status.PASSED);
+    }
+}
